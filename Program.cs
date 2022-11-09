e0f1acc7c3b538601b0cfa7b0a67bb632076d5fe
boodscode237 <boodscode@gmail.com>
2022-11-10 03:43:59 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string continueResult;
+            do
+            {
+                Console.WriteLine("What is your first name: ");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine($"Hello {firstName}");
+                
+                Console.Write("Do you want to continue (yes/no): ");
+                continueResult = Console.ReadLine();
+            } while (continueResult?.ToLower() == "yes");
+
+            Console.Write("What is your age: ");
+            string ageText = Console.ReadLine();
+
+            bool isValidAge = int.TryParse(ageText, out int age);
+
+            while (isValidAge == false)
+            {
+                Console.WriteLine("That was an invalid age. Please type again");
+                isValidAge = int.TryParse(ageText, out age);
+            }
+
+            Console.WriteLine($"Your age in 10 years will be {age + 10}");
+
+            Console.ReadLine();
+        }
+    }
+}
